c8d6294a57a0c402f3ceee6c3ca600034daf6db2
FunGuava <67332582+FunGuava@users.noreply.github.com>
2022-09-14 21:01:48 -0400

    Add basic data type conversion methods
    - this will give people a refresher on how to convert data types and help people debug various errors.

--- meetingNotes/dataTypeIntro.py
+++ meetingNotes/dataTypeIntro.py
@@ -60,3 +60,27 @@ print(5, "5.5")
 print(str(5) + "5.5")
 print(f"{5}5.5") # only works in Python 2
 print("{}5.5".format(5))
+
+#Understanding basic data type converters
+#type() outputs the type of the object you provide. example shown below.
+message = "This is a string :)"
+number = 12345
+print(type(message))
+print(type(number))
+
+#str() converts data type to a string. example shown below.
+age = 31
+print("Python is " + str(age) + " years old!")
+
+#int() converts data type to an integer. example shown below.
+currentAge = input("Enter your current age ") #input function always returns string
+futureAge = int(currentAge) + 10 
+print("You will be " + str(futureAge) + " years old in 10 years")
+
+#float() converts data type to a float. example shown below.
+money = float(input("How much money do you have"))
+pencilCost = 0.50
+folderCost = 1.89
+notebookCost = 2.56
+totalCost = money - pencilCost - folderCost - notebookCost
+print("Your total after buying supplies is " + str(totalCost) + " dollars")
