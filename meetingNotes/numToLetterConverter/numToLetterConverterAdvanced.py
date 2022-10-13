17ba9e661bee52659473726f9ca2b47e62d45b14
battler82004 <34427808+battler82004@users.noreply.github.com>
2022-10-12 23:40:48 -0400

    Update numToLetterConverterAdvanced.py

--- meetingNotes/numToLetterConverter/numToLetterConverterAdvanced.py
+++ meetingNotes/numToLetterConverter/numToLetterConverterAdvanced.py
@@ -1,10 +1,18 @@
+# James Taddei
+# Number to Letter Converter
+# 10/12/22
+
+# Variable declaration
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# User input
 num = int(input("Enter num: "))
 num -= 65
 
+# Final output
 if (num < 0):
     print("Num too small")
 elif (num > 25):
     print("Num too big")
-else:
+else: # Valid number
     print(alphabet[num])
