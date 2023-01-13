4862c163c25bec3781048a81b332e21eb4f7e42d
battler82004 <battler82004@gmail.com>
2023-01-13 15:52:41 -0500

    ADD: main.py and jtahsInfo.py

---
+++ jtModule/jtahsInfo.py
@@ -0,0 +1,8 @@
+# James Taddei
+# JTAHS Info
+# 1/13/23
+
+name = "Jim Thorpe Area High School"
+district = "Jim Thorpe Area School District"
+address = "1 Olympian Way, Jim Thorpe, PA"
+phoneNumer = "(570) 325-3663"
---
+++ jtModule/main.py
@@ -0,0 +1,13 @@
+# James Taddei
+# JT Module
+# 1/13/23
+
+import jtahsInfo
+
+def main():
+    print(f"Name: {jtahsInfo.name}")
+    print(f"District: {jtahsInfo.district}")
+    print(f"Address: {jtahsInfo.address}")
+    print(f"Phone number: {jtahsInfo.phoneNumer}")
+
+main()
